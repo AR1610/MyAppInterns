c666235e67da9713c70984888186c1e07a28d297
Aarti Patel <royal.arati10@gmail.com>
2022-08-31 23:03:10 +0530

    Splash Activity in android

---
+++ app/src/main/java/com/myappinterns/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.myappinterns;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    int time = 3000;
+    GifImageView gifImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        gifImageView = findViewById(R.id.img_gif);
+        gifImageView.setGifImageResource(R.drawable.source);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashActivity.this, AlertActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, time);
+    }
+}
