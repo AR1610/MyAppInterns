78ce073ee901194afe25074631a6c8bc3be05ceb
Aarti Patel <royal.arati10@gmail.com>
2022-09-06 06:51:11 +0530

    DatePicker Dialog in Android

--- app/src/main/java/com/myappinterns/DateTimeActivity.java
+++ app/src/main/java/com/myappinterns/DateTimeActivity.java
@@ -3,19 +3,23 @@ package com.myappinterns;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 public class DateTimeActivity extends AppCompatActivity {
 
-    TextView tvDate;
-    Button btnDate;
+    TextView tvDate,tvTime;
+    Button btnDate,btnTime;
     private int year,month,date;
+    private int hours;
+    private int minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +28,29 @@ public class DateTimeActivity extends AppCompatActivity {
         tvDate = findViewById(R.id.tv_date);
         btnDate = findViewById(R.id.btn_date);
 
+        tvTime = findViewById(R.id.tv_time);
+        btnTime = findViewById(R.id.btn_time);
+
+
         Calendar calendar = Calendar.getInstance();
         year =  calendar.get(Calendar.YEAR);
         month  = calendar.get(Calendar.MONTH);
         date = calendar.get(Calendar.DATE);
+        hours = calendar.get(Calendar.HOUR);
+        minute = calendar.get(Calendar.MINUTE);
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(DateTimeActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        tvTime.setText(i+":"+i1);
+                    }
+                },hours,minute,true);
+                timePickerDialog.show();
+            }
+        });
 
 
         btnDate.setOnClickListener(new View.OnClickListener() {
